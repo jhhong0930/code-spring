158056e833aa116f03719ff25f826899ee8d5309
Joe <76515226+jhhong0930@users.noreply.github.com>
2021-06-13 20:15:14 +0900

    Add files via upload

--- spring/src/main/java/com/spring/controller/SampleController.java
+++ spring/src/main/java/com/spring/controller/SampleController.java
@@ -2,13 +2,15 @@ package com.spring.controller;
 
 import com.spring.domain.SampleDTO;
 import com.spring.domain.SampleDTOList;
+import com.spring.domain.TodoDTO;
 import lombok.extern.java.Log;
 import lombok.extern.log4j.Log4j;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 @Controller
@@ -50,4 +52,39 @@ public class SampleController {
         log.info("list dtos: " + list);
         return "ex02Bean";
     }
+
+//    @InitBinder
+//    public void initBinder(WebDataBinder binder) {
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+//        binder.registerCustomEditor(java.util.Date.class, new CustomDateEditor(dateFormat, false));
+//    }
+
+    @GetMapping("/ex03")
+    public String ex03(TodoDTO todo) {
+        log.info("todo: " + todo);
+        return "ex03";
+    }
+
+    @GetMapping("/ex04")
+    public String ex04(SampleDTO dto, @ModelAttribute("page") int page) {
+        log.info("dto: " + dto);
+        log.info("page: " + page);
+        return "/sample/ex04";
+    }
+
+    @GetMapping("/ex05")
+    public void ex05(SampleDTO dto) {
+        log.info("dto: " + dto);
+        log.info("/ex05..........");
+    }
+
+    @GetMapping("/ex06")
+    public @ResponseBody SampleDTO ex06() {
+        log.info("/ex06...........");
+        SampleDTO dto = new SampleDTO();
+        dto.setAge(10);
+        dto.setName("홍길동");
+        return dto;
+    }
+
 }
---
+++ spring/src/main/webapp/WEB-INF/views/sample/ex05.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: joe
+  Date: 2021/06/13
+  Time: 11:22 오전
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    hello World!
+    <h2>SAMPLEDTO ${sampleDTO}</h2>
+
+</body>
+</html>
